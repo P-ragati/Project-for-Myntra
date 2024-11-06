4cbf2940b8c8235a231bc808ac08862d0a0d877b
P-ragati <pragatimittal31@gmail.com>
2024-11-06 11:23:20 +0530

    files added

---
+++ headerMenu.js
@@ -0,0 +1,27 @@
+
+//<------- TOGGLE MENU JS ----------->//
+function myFunction() {
+  var dropDownMenu = document.getElementById("myDropdown")
+  dropDownMenu.classList.toggle("show")
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+//<---------- HYPERLINK FOR PROFILE ---------->//
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('profile').addEventListener('click', function() {
+      window.location.href = "profile.html";
+  });
+});
